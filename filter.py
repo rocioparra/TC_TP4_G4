a9e4a2c9128bfc9db790013df2f7a5ace38a15f3
taomasgonzalez <togonzalez@itba.edu.ar>
2018-10-07 04:39:35 -0300

    Agregue funciones para desnormalizar y comentarios

--- filter.py
+++ filter.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 import math
 import cmath
-import numpy
+import numpy as np
 from scipy import signal
 
 
@@ -11,7 +11,7 @@ class Filter(ABC):
         super().__init__()
         self.type = filter_type
         self.approx = approx
-        self.n = n # ver que onda cuando lo calculo yo vs cuando me lo imponen
+        self.n = n  # # ver que onda cuando lo calculo yo vs cuando me lo imponen
         self.alphaP = alphaP
         self.alphaA = alphaA
         self.poles = []
@@ -24,17 +24,67 @@ class Filter(ABC):
     def normalize(self):
         pass
 
-    @abstractmethod
+    #me deja en self poles y en self.zeroes todos los ceros y polos resultantes de desnormalizar
     def denormalize(self):
+        self.k = 1
+        final_denorm_poles = []
+        final_denorm_zeroes = []
+
+        for p in self.poles:
+            [denorm_poles, denorm_zeroes, gain_factor] = self.denormalize_one_pole(p)
+            # si se puede apependear tod o junto mejor, no lo se hacer en sintaxis
+            final_denorm_poles.append(denorm_poles)
+            final_denorm_zeroes.append(denorm_zeroes)
+            self.k = self.k * gain_factor
+
+        self.poles = self.add_only_one_complex(final_denorm_poles)
+        self.zeros = self.add_only_one_complex(final_denorm_zeroes)
+
+    # --------------------------
+    # denormalize_one_pole
+    # -------------------------
+    # desnormalize_one_pole es un metodo abstracto, depende del tipo de filtro a utilizar
+    # aplica el cambio de variable apropiado a un solo polo real o a un par conjugado
+    # devuelve [denorm_poles, denorm_zeroes, gain_factor]
+
+    #INPUT:
+        # 1) pole: el polo a desnormalizar,  puede ser un complejo, en cuyo caso contara como tambien haber mandado su conjugado.
+    #OUTPUT:
+        # 1) denorm_poles: polos resultantes de desnormalizar. DEVUELVE DOS COMPLEJOS CONJUGADOS EN CASO DE ESTAR EN ESTA OPCION
+                        # debera eliminarse el conjugado en caso de no necesitarlo!!!
+        # 2) denorm_zeroes: ceros resultantes de desnormalizar. DEVUELVE DOS COMPLEJOS CONJUGADOS EN CASO DE ESTAR EN ESTA OPCION
+                        # debera eliminarse el conjugado en caso de no necesitarlo!!!
+        # 3) gain_factor: la ganancia resultante del cambio de variable para ese polo.
+
+    @abstractmethod
+    def denormalize_one_pole(self, pole):
         pass
 
+
     def calculate_poles(self):
-        self.calculate_norm_poles() ## reemplazar por switcher para approx
-#        self.poles = [p/self.wp for p in self.poles]
-
-        denorm = switcher.get(self.type)
-        [self.poles, self.zeros, self.k] = denorm(self.poles)
+        self.normalize()     # obtengo polos para la normalizada
+        self.add_only_one_complex(self.poles)  # elimino uno de los dos complejos conjugados
+        self.add_only_one_complex(self.zeros)
 
 
+        self.denormalize()      #desnormalizo todos los polos
 
+    # --------------------------
+    # add_only_one_complex
+    # -------------------------
+    #recorre la lista complex_nums, reconoce los numeros pertenecientes a C-R
+    #(parte imaginaria distinta de cero)
+    #de aqui elimina de la lista el complejo con parte imaginaria negativa y
+    #deja su conjugado en la lista.
+
+    # INPUT:
+        # 1) complex_nums: lista con numeros a la cual se le eliminara cualquier complejo con parte imaginaria negativa
+    # OUTPUT:
+        # void.
+    def add_only_one_complex(self, complex_nums):
+        for comp in complex_nums:
+            if comp.imag <0:
+                complex_nums.remove(comp)
+            else:
+                pass
 
--- filtertypes.py
+++ filtertypes.py
@@ -1,5 +1,5 @@
 import filter
-
+import numpy as np
 class LowPass(filter.Filter):
     def __init__(self, approx, template, alphaP, alphaA, n):
         filter.Filter.__init__(self, "lp", approx, alphaP, alphaA, n)
@@ -24,24 +24,32 @@ class HighPass(filter.Filter):
     def normalize(self):
         pass
 
-    def denormalize(self)
+    def denormalize(self, pole):
         ## cambio de variable: s-> wp/s
-        ## 1/(s/p+1) -> (p/wp) * s / (s/(wp/p) + 1)
-        ## entonces por cada polo en p tengo:
+        ## 1/(s-p) ->
+        ## entonces por cada polo REAL en poles tengo:
         ## - un cero en el origen
-        ## - un polo en wp/p
-        ## - ganancia p/wp
-        self.zeros = numpy.zeros(norm_poles.len())
-        self.k = wp ** (-norm_poles.len())
+        ## - un polo en
+        ## - ganancia
 
         denorm_poles = []
-        for p in self.poles:
-            denorm_poles.append(wp / p)
-            self.k *= p
+        denorm_zeroes = []
 
-        self.poles = denorm_poles
+        if pole.imag == 0:
+            denorm_poles = []
+            denorm_zeroes = []
+            gain_factor = 1
+            pass
+        else:
+            denorm_poles = []
+            denorm_zeroes = []
+            gain_factor = 1
+            pass
 
 
+
+
+        return [denorm_poles,denorm_zeroes, gain_factor]
 
 class BandPass(filter.Filter):
     def __init__(self, approx, template, alphaP, alphaA, n):
@@ -52,8 +60,41 @@ class BandPass(filter.Filter):
     def normalize(self):
         pass
 
-    def denormalize(self):
-        pass
+    def denormalize_one_pole(self, pole):
+        ## cambio de variable: s -> q*(s/wo+wo/s) = q*(s^2+wo^2)/(s*wo)
+
+        ## 1/(s-p) -> (wo/p)*s / (s^2 - p*wo/q*s +wo^2)
+        ## entonces por cada polo REAL tengo:
+        ## - un cero en el origen
+        ## - los dos polos que son raices de (s^2 - p*wo/q*s +wo^2)
+        ## - ganancia wo/p
+
+        denorm_poles = []
+        denorm_zeroes = []
+
+        if pole.imag == 0:
+            denorm_poles = np.roots([1, -pole * self.wo /self.q, self.wo ** 2])
+            denorm_zeroes = 0
+            gain_factor = self.wo/self.p
+
+        ## 1/( (s-p)*(s-conj(p)) ) -> (wo^2/q)*s^2 / ( s^4 - 2*wo*Re(p)/q *s^3 + (2*q^2+mod(p)^2/q^2)*wo^2 *s^2 - 2*wo^3*Re(p)/q *s + wo^4 )
+        ## entonces por cada polo IMAGINARIO tengo:
+        ## - dos ceros en el origen
+        ## - los polos que son raices de s^4 - 2*wo*Re(p)/q *s^3 + (2*q^2+mod(p)^2/q^2)*wo^2 *s^2 - 2*wo^3*Re(p)/q *s + wo^4
+        ## esto puede ser escrito como s^4 + a *s^3 + b *s^2 + c *s + d
+        ## - ganancia wo^2/q
+
+        else:
+            a = - 2 * self.wo *np.real(pole) /self.q
+            b = (2 * self.**2 + abs(pole)^2 / self.q**2 ) *self.wo**2
+            c = - 2 * self.wo**3 * np.real(pole) / self.q
+            d = self.w0 ** 4
+
+            denorm_poles = np.roots([1, a, b, c, d])
+            denorm_zeroes = [0, 0]
+            gain_factor = self.wo**2 /self.q
+
+        return [denorm_poles, denorm_zeroes, gain_factor]
 
 
 class BandReject(filter.Filter):
@@ -65,5 +106,5 @@ class BandReject(filter.Filter):
     def normalize(self):
         pass
 
-    def denormalize(self):
+    def denormalize_one_pole(self, pole):
         pass
