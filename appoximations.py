6ba4e927fb6d1bef3a024d168011b515e15cc044
rocioparra <rparra@itba.edu.ar>
2018-10-15 09:15:05 -0300

    cheby 1 y 2 andando

--- appoximations.py
+++ appoximations.py
@@ -6,106 +6,123 @@ from interface import implements, Interface, default
 
 class Approximation(Interface):
     @staticmethod
-    def get_min_n(self, wa_norm, alpha_a, alpha_p):
+    def get_min_n(wa_norm, alpha_a, alpha_p):
         pass
 
     @staticmethod
-    def pzk(self, n, alpha_p):
+    def pzk(n, alpha_p, wa_norm):
     # devuelve los polos y ceros con im()>0. como las funciones que definen son a coefs ctes reales, agregar el conjugado
         pass
 
     @default
-    def epsilon(self, alpha_p):
+    @staticmethod
+    def epsilon(alpha_p):
         return math.sqrt(10**(alpha_p/10)-1)
 
 
 class Butterworth(implements(Approximation)):
     @staticmethod
-    def get_min_n(self, wa_norm, alpha_a, alpha_p):
-        epsilon = self.epsilon(alpha_p)
+    def get_min_n(wa_norm, alpha_a, alpha_p):
+        epsilon = Butterworth.epsilon(alpha_p)
         return math.ceil(math.log10(math.sqrt((10**(alpha_a/10)-1))/epsilon)/math.log10(wa_norm))
 
     @staticmethod
-    def pzk(self, n, alpha_p):
-        epsilon = self.epsilon(alpha_p)
+    def pzk(n, alpha_p, wa_norm):
+        epsilon = Butterworth.epsilon(alpha_p)
         poles = []
         r = epsilon**(-1/n)
         delta_theta = math.pi/n
         theta = math.pi/2 + delta_theta/2
+        k = 1
 
-        while len(poles) < n//2: # agrego los polos con im(p)>0
-            poles.append(cmath.rect(r, theta))
+        while len(poles) < n//2:# agrego los polos con im(p)>0
+            p = cmath.rect(r, theta)
+            poles.append(p)
             theta += delta_theta
+            k *= numpy.absolute(p)**2
 
-        if len(poles) == n-1: # si me falta un polo esta en el eje real
+        if n % 2 == 1:  # si me falta un polo esta en el eje real
             poles.append(-r)
+            k *= (-r)
 
-        return [poles, [], 1]
+        return [poles, [], k]
 
 
 class Chebyshev(implements(Approximation)):
     @staticmethod
-    def get_min_n(self, wa_norm, alpha_a, alpha_p):
-        epsilon = self.epsilon(alpha_p)
+    def get_min_n(wa_norm, alpha_a, alpha_p):
+        epsilon = Chebyshev.epsilon(alpha_p)
         n = math.acosh(math.sqrt(10**(alpha_a/10))/epsilon)/math.acosh(wa_norm)
-        return n
+        return math.ceil(n)
 
     @staticmethod
-    def pzk(self, n, alpha_p):
-        epsilon = self.epsilon(alpha_p)
-        beta = math.asinh(1/epsilon)/n
+    def pzk(n, alpha_p, wa_norm):
+        epsilon = Chebyshev.epsilon(alpha_p)
+        beta = -math.asinh(1 / epsilon) / n
+        gain = 1
 
         poles = []
-        for k in range(1, n//2+1):
-            alpha = (2*k - 1) * math.pi / 2 / n
-            re = math.sen(alpha) * math.sinh(beta)
+        for k in range(1, n // 2 + 1):
+            alpha = (2 * k - 1) * math.pi / 2 / n
+            re = math.sin(alpha) * math.sinh(beta)
             im = math.cos(alpha) * math.cosh(beta)
-            poles.append(re + 1j*im)
+            p = re + 1j * im
+            poles.append(p)
+            gain *= (numpy.absolute(p) ** 2)
 
-        if len(poles) < math.ceil(n/2):
-            poles.append(-abs(math.sinh(beta)))
-
-        return [poles, [], 1]
+        if n % 2 == 1:
+            p = math.sinh(beta)
+            poles.append(p)
+            gain *= (-p)
+        else:
+            gain *= 10 ** (-alpha_p / 20)
+        return [poles, [], gain]
 
 
 class InvChebyshev(implements(Approximation)):
     @staticmethod
-    def get_min_n(self, wa_norm, alpha_a, alpha_p):
-    ## A ESTE PASARLE WP_NORM, WA_NORM = 1
-        epsilon = 1/self.epsilon(alpha_a)
-        return math.acosh(1/(epsilon * math.sqrt(10**(alpha_p/10)-1)))/math.acosh(1/wa_norm)
-        pass
+    def get_min_n(wa_norm, alpha_a, alpha_p):
+        return Chebyshev.get_min_n(wa_norm=wa_norm, alpha_a=alpha_a, alpha_p=alpha_p)
 
     @staticmethod
-    def pzk(self, n, alpha_p):
-        epsilon = 1/self.epsilon(alpha_a)
-        beta = math.asinh(1/epsilon)/n
+    def epsilon(alpha_p): ##ESTE RECIBE ALPHA_A, NO P. PERO TIENE QUE TENER EL MISMO NOMBRE LA VARIABLE
+        return 1/math.sqrt(10**(alpha_p/10)-1)
 
+    @staticmethod
+    def pzk(n, alpha_p, wa_norm): #PASARLE ALPHA_A, NO ALPHA_P
+        epsilon = InvChebyshev.epsilon(alpha_p)
+        beta = -math.asinh(1 / epsilon) / n
+        gain = 1
         poles = []
         zeros = []
 
-        for k in range(1, n//2+1):
+        for k in range(1, n // 2 + 1):
             alpha = (2 * k - 1) * math.pi / 2 / n
-            rec1 = math.sen(alpha) * math.sinh(beta)
-            imc1 = math.cos(alpha) * math.cosh(beta)
+            re = math.sin(alpha) * math.sinh(beta)
+            im = math.cos(alpha) * math.cosh(beta)
+            p = wa_norm/(re - 1j * im)
+            poles.append(p)
+            gain *= (numpy.absolute(p) ** 2)
 
-            poles.append(1/(rec1 - 1j * imc1))
-            zeros.append(-1j / math.cos(alpha))
+            z = 1j * wa_norm / math.cos(alpha)
+            zeros.append(z)
+            gain /= numpy.absolute(z)**2
 
-        if len(poles) < math.ceil(n/2):
-            poles.append(-abs(1/math.sinh(beta)))
-            zeros.append(1j)
+        if n % 2 == 1:
+            p = wa_norm/math.sinh(beta)
+            poles.append(p)
+            gain *= (-p)
 
-        return [poles, zeros, 1]
+        return [poles, zeros, gain]
 
 
 class Bessel(implements(Approximation)):
     @staticmethod
-    def get_min_n(self, wa_norm, alpha_a, alpha_p):
+    def get_min_n(wa_norm, alpha_a, alpha_p):
         pass
 
     @staticmethod
-    def pzk(self, n, alpha_p):
+    def pzk(n, alpha_p, wa_norm):
         pass
 
     @staticmethod
@@ -116,6 +133,6 @@ class Bessel(implements(Approximation)):
             return [1, 3, 1]
         else:
             b_n_1 = (2*n-1) * Bessel.get_coeffients(n-1)
-            b_n_2 = numpy.convolve(Bessel.get_coeffients(n-2), [1, 0 ,0])
+            b_n_2 = numpy.convolve(Bessel.get_coeffients(n-2), [1, 0,0])
             b_n_1 = numpy.pad(b_n_1, (len(b_n_2)-len(b_n_1), 0), 'constant', constant_values=0)
             return b_n_1 + b_n_2
--- test_approx.py
+++ test_approx.py
@@ -1,22 +1,47 @@
-from appoximations import Butterworth
+from appoximations import Butterworth, Chebyshev, InvChebyshev
 from scipy import signal
+import matplotlib.pyplot as plt
 import math
+import numpy
 
 wp = 1
-wa = 20
+wa = 2
 alpha_p = 10*math.log10(2)
-alpha_a = 100
+alpha_a = 10
 
-butter = Butterworth()
-n = butter.get_min_n(butter, wp=wp, wa=wa, alpha_a=alpha_a, alpha_p=alpha_p)
-[p, z, k] = butter.pzk(butter, alpha_p=alpha_p, n=n)
+# approx = Butterworth()
+# approx = Chebyshev()
+approx = InvChebyshev()
+
+n = approx.get_min_n(wa_norm=wa, alpha_a=alpha_a, alpha_p=alpha_p)
+[ppos, zpos, k] = approx.pzk(alpha_p=alpha_a, n=n, wa_norm=wa)
+p = []
+z = []
+if n % 2 == 1:
+    p.append(ppos.pop(len(ppos)-1))
+for i in range(len(ppos)):
+    p.append(ppos[i])
+    p.append(numpy.conj(ppos[i]))
+for i in range(len(zpos)):
+    z.append(zpos[i])
+    z.append(numpy.conj(zpos[i]))
+
 sys = signal.ZerosPolesGain(z, p, k)
 Hmia = sys.to_tf()
 
 
-[N, D] = signal.butter(N=n, Wn=wp, output='ba', analog=True)
+[N, D] = signal.cheby2(N=n, rs=alpha_a, Wn=wa, output='ba', analog=True)
 Hposta = signal.TransferFunction(N, D)
 
+[w, mag, pha] = signal.bode(Hmia)
+plt.semilogx(w, -mag)
+plt.grid(b=True)
+plt.show()
+
+[w, mag, pha] = signal.bode(Hposta)
+plt.semilogx(w, -mag)
+plt.grid(b=True)
+plt.show()
 
 print(Hposta.num, Hposta.den)
 print(Hmia.num, Hmia.den)
