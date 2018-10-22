9f23dc992829328002064b6d6cb49da140fd2aa0
taomasgonzalez <togonzalez@itba.edu.ar>
2018-10-22 01:33:53 -0300

    adsasd

--- test_approx.py
+++ test_approx.py
@@ -5,6 +5,7 @@ import math
 import numpy
 from filter_template import GroupDelayTemplate
 
+
 # wp = 1
 # wa = 2
 # alpha_p = 10*math.log10(2)
@@ -47,27 +48,27 @@ from filter_template import GroupDelayTemplate
 # print(Hposta.num, Hposta.den)
 # print(Hmia.num, Hmia.den)
 
-t = GroupDelayTemplate(w_rg=6, tolerance=.20)
-approx = Bessel()
-
-n = approx.get_min_n(t)
-[ppos, zpos, k] = approx.pzk(n, t)
-p = []
-z = []
-if n % 2 == 1:
-    p.append(ppos.pop(len(ppos)-1))
-for i in range(len(ppos)):
-    p.append(ppos[i])
-    p.append(numpy.conj(ppos[i]))
-for i in range(len(zpos)):
-    z.append(zpos[i])
-    z.append(numpy.conj(zpos[i]))
-
-sys = signal.ZerosPolesGain(z, p, k)
-Hmia = sys.to_tf()
-[w, mag, pha] = signal.bode(Hmia)
-plt.semilogx(w, -mag)
-plt.grid(b=True)
-plt.show()
+# t = GroupDelayTemplate(w_rg=6, tolerance=.20)
+# approx = Bessel()
+#
+# n = approx.get_min_n(t)
+# [ppos, zpos, k] = approx.pzk(n, t)
+# p = []
+# z = []
+# if n % 2 == 1:
+#     p.append(ppos.pop(len(ppos)-1))
+# for i in range(len(ppos)):
+#     p.append(ppos[i])
+#     p.append(numpy.conj(ppos[i]))
+# for i in range(len(zpos)):
+#     z.append(zpos[i])
+#     z.append(numpy.conj(zpos[i]))
+#
+# sys = signal.ZerosPolesGain(z, p, k)
+# Hmia = sys.to_tf()
+# [w, mag, pha] = signal.bode(Hmia)
+# plt.semilogx(w, -mag)
+# plt.grid(b=True)
+# plt.show()
 
 
